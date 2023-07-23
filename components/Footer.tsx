e4d4f44e753ce249e9c91676555f5325ed6cc0b9
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-07-23 00:52:02 -0300

    fix: changed footer text color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,12 @@ export function Footer() {
   return (
     <footer className="mt-4 w-full">
       <div className="flex flex-col gap-2 max-w-screen-lg mx-auto">
-        <hr className="border-text-secondary border-opacity-50" />
+        <hr className="border-text-tertiary border-opacity-50" />
         <div
           className="flex flex-col sm:flex-row m-2 items-center justify-center 
           sm:justify-between gap-4 sm:mx-5 sm:my-3"
         >
-          <p className="text-text-secondary text-xs sm:text-sm text-center">
+          <p className="text-text-tertiary text-xs sm:text-sm text-center">
             Made with ❤️ and ☕ by Gustavo Maltez. <br />
             © 2023 Gustavo Maltez. All rights reserved. <br />
           </p>
