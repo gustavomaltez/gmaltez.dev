051a4245f3306efe4a0fde2ee574794cb939cb1b
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-07-22 22:09:21 -0300

    feat: added new 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -3,9 +3,44 @@ import { Wrapper } from "../components/Wrapper.tsx";
 
 export default function NotFoundPage(props: UnknownPageProps) {
   return (
-    <Wrapper>
+    <Wrapper
+      head={
+        <>
+          <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@700&family=Montserrat:wght@900&display=swap" rel="stylesheet" />
+          <style>
+            {`
+            .not-found-text {
+              font-family: cabin,sans-serif;
+              color: #d0ccc6;
+              text-transform: uppercase;
+              text-align: center;
+              font-size: 1.25rem;
+            }
+
+            .not-found-numbers {
+              font-weight: 900;
+              letter-spacing: -1rem;
+              font-family: montserrat,sans-serif;
+              color: #d0ccc6;
+              text-shadow: #191c23 -8px 0px 0px;
+            }
+          `}
+          </style>
+        </>
+      }
+    >
       <div className="flex flex-1 lg:w-screen max-w-screen-lg h-full items-center justify-center">
-        <img src="/404.svg" className="p-5 w-full h-full max-h-[70vh]"/>
+        <div className="flex flex-col items-center justify-center max-w-md">
+          <h3 className="not-found-text text-lg sm:text-xl">Oops! Page not found!</h3>
+          <h1 className="not-found-numbers text-[10rem] -my-[2.5rem] sm:text-[14rem] sm:-my-[3.5rem]">
+            <span>4</span>
+            <span>0</span>
+            <span>4</span>
+          </h1>
+          <h2 className="not-found-text text-lg sm:text-xl">
+            Sorry! Seems that the page you're trying to access doesn't exist
+          </h2>
+        </div>
       </div>
     </Wrapper>
   );
