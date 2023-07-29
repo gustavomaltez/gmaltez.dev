c17e0c01ab80a0735ec47b28bac7018335d2b1e6
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-07-29 17:46:29 -0300

    feat: added first draft of post search logic

--- islands/PostSearch.tsx
+++ islands/PostSearch.tsx
@@ -1,36 +1,165 @@
 import { Post } from '@utils/posts.ts';
-import { useState } from 'https://esm.sh/preact@10.13.1/hooks';
+import { useReducer } from 'https://esm.sh/preact@10.13.1/hooks';
 
 import { PostPreview, SearchBar } from '../components/index.ts';
 
-export default function PostSearch(props: { posts: Post[]; }) {
-  const [posts, setPosts] = useState(props.posts);
+// ToDo: Refactor this component, clear the code and make it more readable.
 
+export default function PostSearch(props: { posts: Post[]; }) {
+  const [state, dispatch] = useReducer(searchReducer, buildInitialReducerState(props.posts));
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex flex-col md:flex-row w-full">
+      <div className="flex flex-col mobile-high:flex-row w-full">
         <SearchBar
-          onClear={() => setPosts(props.posts)}
-          onInput={e => {
-            const query = e.currentTarget.value.toLowerCase();
-            if (query.length === 0) return setPosts(props.posts);
-            setPosts(
-              props.posts.filter(
-                post =>
-                  post.title.toLowerCase().includes(query) ||
-                  post.content.toLowerCase().includes(query) ||
-                  post.tags.some(tag => tag.toLowerCase().includes(query))
-              )
-            );
-          }}
+          onClear={() => dispatch({ type: 'CLEAR_QUERY' })}
+          onInput={e => dispatch({ type: 'UPDATE_QUERY', query: e.currentTarget.value.toLowerCase() })}
         />
-        <button className="basis-1/5 mt-2 md:mt-0 md:ml-2 bg-primary text-white px-4 py-2 rounded-md min-w-max">
-          Show Tags
+        <button
+          className="basis-1/5 mt-2 mobile-high:mt-0 mobile-high:ml-2 bg-primary 
+          text-white px-4 py-2 rounded-lg outline-none min-w-[120px]"
+          onClick={() => dispatch({ type: 'TOGGLE_TAGS', state: !state.isTagsVisible })}
+        >
+          {state.isTagsVisible ? 'Hide Tags' : 'Show Tags'}
         </button>
       </div>
+      {state.isTagsVisible && (
+        <div className="flex flex-row flex-wrap mt-2 justify-center">
+          {state.tags.map(({ tag, isSelected }) => (
+            <button
+              className={`px-4 py-2 rounded-lg transition-all duration-200 mr-2 mb-2
+              bg-background-secondary hover:bg-primary hover:text-white
+              ${isSelected
+                  ? 'text-white border border-primary'
+                  : 'text-text-primary border border-background-secondary'}`}
+              onClick={() => dispatch({ type: 'TAG_CLICK', tag })}
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+      )}
       <div className="flex flex-col gap-5 mt-5">
-        {posts.map(post => <PostPreview {...post} />)}
+        {state.filteredPosts.map(post => <PostPreview {...post} />)}
       </div>
     </div>
   );
 }
+
+// Reducer ---------------------------------------------------------------------
+
+function searchReducer(state: State, action: Action) {
+  switch (action.type) {
+    case 'TOGGLE_TAGS':
+      return { ...state, isTagsVisible: action.state };
+    case 'CLEAR_QUERY':
+      return { ...state, query: '', isQueryEmpty: true, ...updatePosts('', state.tags) };
+    case 'TAG_CLICK': {
+      const tags = handleTagClick(action.tag);
+      return { ...state, ...action, ...updatePosts(state.query, tags), tags };
+    }
+    case 'UPDATE_QUERY':
+      return { ...state, ...action, ...isQueryEmpty(action.query), ...updatePosts(action.query) };
+  }
+
+  function isQueryEmpty(query: string) {
+    return { isQueryEmpty: query.trim().length === 0 };
+  }
+
+  function getFilteredPosts(query: string, tags?: Tag[]) {
+    if (!query && !tags) return [];
+    const result = state.posts.filter(
+      post =>
+        post.title.toLowerCase().includes(query) ||
+        post.snippet.toLowerCase().includes(query) ||
+        post.tags.some(tag => tag.toLowerCase().includes(query))
+    );
+    if (!tags) return result;
+    const selectedTags = getSelectedTags(tags);
+    return filterPostsByTags(result, selectedTags);
+  }
+
+  function updatePosts(query: string, tags?: Tag[]) {
+    const filteredPosts = getFilteredPosts(query, tags);
+    return { filteredPosts, noPostsFound: filteredPosts.length === 0 };
+  }
+  function handleTagClick(tag: string) {
+    const tags = state.tags.map(({ tag: _tag, isSelected }) => ({
+      tag: _tag,
+      isSelected: _tag === tag ? !isSelected : isSelected,
+    }));
+    setTagsToUrl(getSelectedTags(tags));
+    return tags;
+  }
+}
+
+function buildInitialReducerState(posts: Post[]) {
+  const tags = getInitialTags(posts);
+  const selectedTags = getSelectedTags(tags);
+  return {
+    posts,
+    query: '',
+    isQueryEmpty: true,
+    noPostsFound: false,
+    tags: getInitialTags(posts),
+    isTagsVisible: selectedTags.length > 0,
+    filteredPosts: filterPostsByTags(posts, selectedTags),
+  };
+}
+
+// Helpers ---------------------------------------------------------------------
+
+function getTagsFromUrl() {
+  if (!window?.location) return [];
+  const url = new URL(window.location.href);
+  const tags = url.searchParams.get('tags');
+  if (tags === null) return [];
+  return tags.split(',');
+}
+
+function setTagsToUrl(tags: string[]) {
+  if (!window?.location) return;
+  const url = new URL(window.location.href);
+  url.searchParams.set('tags', tags.join(','));
+  window.history.pushState({}, '', url.toString());
+  if (tags.length === 0) return url.searchParams.delete('tags');
+}
+
+function getTagsFromPosts(posts: Post[]) {
+  const tags = new Set<string>();
+  posts.forEach(post => post.tags.forEach(tag => tags.add(tag)));
+  return Array.from(tags);
+}
+
+function getInitialTags(posts: Post[]) {
+  const tags = getTagsFromPosts(posts);
+  const tagsFromUrl = getTagsFromUrl();
+  return tags.map(tag => ({ tag, isSelected: tagsFromUrl.includes(tag) }));
+}
+
+function filterPostsByTags(posts: Post[], tags: string[]) {
+  return posts.filter(post => tags.every(tag => post.tags.includes(tag)));
+}
+
+function getSelectedTags(tags: Tag[]) {
+  return tags.filter(tag => tag.isSelected).map(tag => tag.tag);
+}
+
+// Types -----------------------------------------------------------------------
+
+type Tag = { tag: string; isSelected: boolean; };
+
+type State = {
+  query: string;
+  posts: Post[];
+  tags: Tag[];
+  filteredPosts: Post[];
+  isQueryEmpty: boolean;
+  noPostsFound: boolean;
+  isTagsVisible: boolean;
+};
+
+type Action =
+  | { type: 'CLEAR_QUERY'; }
+  | { type: 'UPDATE_QUERY', query: string; }
+  | { type: 'TOGGLE_TAGS', state: boolean; }
+  | { type: 'TAG_CLICK', tag: string; };
--- routes/posts.tsx
+++ routes/posts.tsx
@@ -16,7 +16,7 @@ export default function Posts(props: PageProps<Post[]>) {
       title='Posts'
     >
       <section className="lg:w-screen max-w-screen-lg flex flex-col flex-1 gap-2">
-        <h1 className="text-3xl font-bold my-2">
+        <h1 className="text-xl mobile-high:text-2xl sm:text-3xl font-bold my-2">
           Search by title, tag or content
         </h1>
         <PostSearch posts={props.data} />
