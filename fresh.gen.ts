3f4a49a8b562e82b99110e7a811280f889b4be59
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-10-18 20:40:10 -0300

    chore: updated fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,10 +5,11 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/blog/[slug].tsx";
-import * as $3 from "./routes/changelog.tsx";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/posts.tsx";
+import * as $2 from "./routes/auth/github.ts";
+import * as $3 from "./routes/blog/[slug].tsx";
+import * as $4 from "./routes/changelog.tsx";
+import * as $5 from "./routes/index.tsx";
+import * as $6 from "./routes/posts.tsx";
 import * as $$0 from "./islands/Changelog.tsx";
 import * as $$1 from "./islands/PostSearch.tsx";
 import * as $$2 from "./islands/ProgressBar.tsx";
@@ -17,10 +18,11 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/about.tsx": $1,
-    "./routes/blog/[slug].tsx": $2,
-    "./routes/changelog.tsx": $3,
-    "./routes/index.tsx": $4,
-    "./routes/posts.tsx": $5,
+    "./routes/auth/github.ts": $2,
+    "./routes/blog/[slug].tsx": $3,
+    "./routes/changelog.tsx": $4,
+    "./routes/index.tsx": $5,
+    "./routes/posts.tsx": $6,
   },
   islands: {
     "./islands/Changelog.tsx": $$0,
