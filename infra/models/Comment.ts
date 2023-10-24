adfda19a37c1d2c1aaa3c2e0762af54a12419fff
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-10-24 20:24:37 -0300

    refactor: improved comment model

--- infra/models/Comment.ts
+++ infra/models/Comment.ts
@@ -1,8 +1,23 @@
 import { User } from "@models";
 
+type MandatoryFields = {
+  id: string;
+  author: User;
+  content: string;
+  createdAt: Date;
+}
+
 export class Comment {
-  public id: string | null = null;
-  public author: User | null = null;
-  public content: string | null = null;
+  public id: string;
+  public author: User;
+  public content: string;
+  public createdAt: Date;
   public parentId: string | null = null;
+
+  constructor(data: MandatoryFields) {
+    this.id = data.id;
+    this.author = data.author;
+    this.content = data.content;
+    this.createdAt = data.createdAt;
+  }
 }
