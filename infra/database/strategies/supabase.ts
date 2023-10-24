35c0f9e859b46f33f8aa632c86bea68cff2b45d8
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-10-24 20:26:38 -0300

    feat: added post repository implementation for supabase

--- infra/database/strategies/supabase.ts
+++ infra/database/strategies/supabase.ts
@@ -1,8 +1,10 @@
 import { SupabaseClient, createClient } from 'supabase';
-import { BaseDatabase } from "@database";
-import { PostRepository } from "../../repositories/PostRepository.ts";
-import { UserRepository } from "@repositories";
-import { User } from "../../models/User.ts";
+
+import { BaseDatabase } from '@database';
+import { Post, User, Comment } from '@models';
+import { isDate, isStringArray } from '@utils/validators.ts';
+import { extractTextMetadataAndContent } from '@utils/files.ts';
+import { UserRepository, PostRepository, PostQuery } from '@repositories';
 
 // Core ------------------------------------------------------------------------
 
@@ -41,8 +43,63 @@ class BaseRepository {
 }
 
 class _PostRepository extends BaseRepository implements PostRepository {
-  public getBySlug(_slug: string) {
-    return Promise.resolve(null);
+
+  public async getBySlug(slug: string, query?: PostQuery) {
+    try {
+      const { metadata, content } = await this._getPostDataBySlug(slug);
+      const { title, snippet, tags, publishedAt } = this._extractPostMetadata(metadata);
+      const post = new Post({ slug, title, content, snippet, publishedAt });
+      post.tags = tags;
+      if (query?.includeComments)
+        post.comments = await this._getCommentsBySlug(slug);
+      return post;
+    } catch (_error) {
+      return null;
+    }
+  }
+
+  public async getAll(query?: PostQuery) {
+    const files = Deno.readDir('./posts');
+    const promises: Promise<Post | null>[] = [];
+    for await (const file of files)
+      promises.push(
+        this.getBySlug(file.name.replace('.md', ''), query)
+      );
+    return (await Promise.all(promises)).filter(Boolean) as Post[];
+  }
+
+  // Helper methods ------------------------------------------------------------
+
+  private async _getCommentsBySlug(slug: string) {
+    const { data, error } = await this.supabase
+      .from('comments')
+      .select('*')
+      .eq('post_slug', slug);
+
+    if (error) throw error;
+    if (!data) return [];
+
+    const comments: Comment[] = [];
+    for (const entry of data) {
+      const { id, content, author } = entry;
+      const createdAt = new Date(entry.created_at);
+      if (isStringArray([id, content, author]) && isDate(createdAt))
+        comments.push(new Comment({ id, content, author, createdAt }));
+    }
+    return comments;
+  }
+
+  private async _getPostDataBySlug(slug: string) {
+    const rawData = await Deno.readTextFile(`./posts/${slug}.md`);
+    return extractTextMetadataAndContent(rawData);
+  }
+
+  private _extractPostMetadata(metadata: Record<string, string>) {
+    const { title, snippet, tags } = metadata;
+    const publishedAt = new Date(metadata.published_at);
+    if (!isStringArray([title, snippet, tags]) || !isDate(publishedAt))
+      throw new Error('Invalid or missing post metadata.');
+    return { title, snippet, tags: metadata.tags.split(','), publishedAt };
   }
 }
 
@@ -70,7 +127,7 @@ class _UserRepository extends BaseRepository implements UserRepository {
 
     if (error) throw error;
     if (!data) throw new Error('User not created');
-    
+
     const user = new User();
     user.id = data.id;
     user.githubId = data.github_id;
