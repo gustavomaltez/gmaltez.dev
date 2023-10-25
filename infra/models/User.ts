56c95addda14775a02692689090554bd86c07280
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-10-24 22:09:26 -0300

    feat: added mandatory fields to user model

--- infra/models/User.ts
+++ infra/models/User.ts
@@ -1,6 +1,18 @@
+type MandatoryFields = {
+  id: string;
+  name: string;
+  githubId: string;
+}
+
 export class User {
   public id: string | null = null;
   public name: string | null = null;
   public githubId: string | null = null;
+
+  constructor(data: MandatoryFields) {
+    this.id = data.id;
+    this.name = data.name;
+    this.githubId = data.githubId;
+  }
 }
 
