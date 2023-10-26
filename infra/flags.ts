f6578bd4beb1843e5fc7336e6a1deba6fcb0f55f
Gustavo Maltez <gustavomaltez37@hotmail.com>
2023-10-25 21:14:16 -0300

    feat: added features enum

---
+++ infra/flags.ts
@@ -0,0 +1,6 @@
+/** Flags that are used to enable/disable features in the application. */
+export enum Feature {
+  COMMENTS = 'COMMENTS',
+  POST_SEARCH = 'POST_SEARCH',
+  CHANGELOG_PAGE = 'CHANGELOG_PAGE',
+}
